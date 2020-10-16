ba3a64e1957ae22e4ddb638231e08117291605f4
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-10-16 10:21:43 +0530

    modified: Functionality of sorting by State,City and Zip are added

--- eclipse-workspace/address/src/address/PersonalContact.java
+++ eclipse-workspace/address/src/address/PersonalContact.java
@@ -1,10 +1,5 @@
 package address;
 
-public class PersonalContact extends Contacts {
-
-	public PersonalContact(String firstName, String lastName, String addressCity, String State, String phoneNumber,
-			String email, String zip) {
-		super(firstName, lastName, addressCity, State, phoneNumber, email, zip);
-	}
+public class PersonalContact {
 
 }
